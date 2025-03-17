973c657efa1b9661ed9b5d537f84f20c29039e91
Louie Olsen <Louie.Ferdinand05@gmail.com>
2025-03-17 14:25:28 +0100

    reorganized tokenizer.h

--- project-root/src/tokenizer.h
+++ project-root/src/tokenizer.h
@@ -11,3 +11,4 @@ int get_token_id(char *token, char vocab[VOCAB_SIZE][MAX_TOKEN_LENGTH], int voca
 int add_token_to_vocab(char *token, char vocab[VOCAB_SIZE][MAX_TOKEN_LENGTH], int *vocab_size);
 
 #endif // TOKENIZER_H
+ 
